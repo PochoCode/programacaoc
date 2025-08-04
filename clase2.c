5878d90b83138c0620cc3c50e3e47858d95244aa
Robin A Rodriguez <robinarodriguez1@gmail.com>
2025-08-04 03:20:58 +0000

    novos comandos

---
+++ clase2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() {
+    // Variaveis para armazenar os números
+ 
+
+    // Solicitar ao usuário que insira os números
+
+   //printf("Digite o primeiro número:\n ");
+   //scanf("%d", &numero1);
+   //printf("Digite o segundo número:\n ");
+   // scanf("%d", &numero2);
+
+    //Variavel para armazenar a suma
+    //int soma = numero1 + numero2;
+
+    //Variavel para armazenar subtração
+    //int subtracao = numero1 - numero2;
+
+
+    //Variavel para armazenar multiplicação
+    //int multiplicacao = numero1 * numero2;
+
+    //Variavel para armazenar divisão
+    //int divisao = numero1 / numero2;
+
+   
+    //Exibir os resultados
+    //printf("Soma: %d\n", soma);
+    //printf("Subtração: %d\n", subtracao);
+    //printf("Multiplicação: %d\n", multiplicacao);
+    //printf("Divisão: %d\n", divisao);
+    
+
+    // Operadores de atribuição
+
+    int numero1 = 10;
+    int numero2;
+    //numero1 += 1;
+    //numero1 -=2;
+    //numero1 *= 3;
+    //numero1 /= 2;
+    //numero1 ++;
+    //numero1 --;
+    //numero2 = numero1++;
+    //numero2 = ++numero1;
+   // numero2 = --numero1;
+    //numero2 = numero1--;
+
+
+    printf("numero1: %d\n", numero1);
+    printf("numero2: %d\n", numero2);
+    return 0;
+
+}
+    
